8738b21c3f464ec6bb054e48a0a3e91f52a4e92f
wardchris061 <32394035+wardchris061@users.noreply.github.com>
2018-10-29 13:24:33 -0400

    Add files via upload
    Weather API functionality and other minor functions for the bot

---
+++ bitcoin.py
@@ -0,0 +1,9 @@
+import requests
+import json
+
+
+def bitcoinVal(): 
+    url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
+    response = requests.get(url)
+    value = response.json()['bpi']['USD']['rate']
+    return value
